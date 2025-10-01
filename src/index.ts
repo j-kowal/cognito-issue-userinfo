675735bfa5d238b188ae9ed04e11714f973d5b8d
j-kowal <janokowal@outlook.com>
2025-10-01 13:10:26 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { Hono } from 'hono'
+import { v4 as uuidv4 } from 'uuid';
+import { logger } from 'hono/logger';
+
+const app = new Hono();
+app.use(logger())
+
+app.get('/userinfo', (c) => {
+  const uuid = uuidv4();
+  const resp = {
+    email: 'john@doe.com',
+    phone_number: '4791758378',
+    family_name: 'Doe',
+    given_name: 'John',
+    sub: uuid,
+    address: {
+      country: 'NO',
+      street: 'Norway Street',
+      houseNumber: '1',
+      floor: undefined,
+      floorType: undefined,
+      entrance: undefined,
+      suite: undefined,
+      postalCode: '1234',
+      postalPlace: 'Oslo'
+    }
+  }
+  console.log(uuid);
+  console.log(resp);
+  return c.json(resp, 200);
+})
+
+export default app
